249b722f7d8ee368a65b1860f728d8c4ce9b5729
ch0ppah <nick0suna76@gmail.com>
2025-04-30 14:24:40 -0400

    update leafnode.py to_html() logic

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,23 +1,25 @@
 from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        if not isinstance(value, str) or not value:
-            raise ValueError("LeafNode must have a value")
         super().__init__(tag, value, None, props)
+        if not isinstance(self.value, str) or not self.value:
+            raise ValueError("LeafNode must have a value")
 
     def to_html(self):
-        if not isinstance(value, str) or not value:
+        if not isinstance(self.value, str) or not self.value:
             raise ValueError("LeafNode must have a value")
         if self.tag == None:
             return self.value
         
         #generates opening tag
-        opening_tag = f"<{tag}"
+        opening_tag = f"<{self.tag}"
         
         #adds properties to tag (if there are any)
-        for key, value in self.props.items():
-            opening_tag += f" {key}=\"{value}\""
+        if self.props:
+            for key, value in self.props.items():
+                opening_tag += f" {key}=\"{value}\""
+        
         opening_tag += ">"
 
         #returns final html string output
-        return opening_tag + self.value + f"</{tag}>"
+        return opening_tag + self.value + f"</{self.tag}>"
