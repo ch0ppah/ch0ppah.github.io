825b3e0b974ee244cadcea0421eee698aa69eb6c
ch0ppah <nick0suna76@gmail.com>
2025-04-30 15:43:02 -0400

    add text_node_to_html_node function

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,3 +1,5 @@
+from textnode import TextNode, TextType
+
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
@@ -19,6 +21,23 @@ class HTMLNode:
             props_str += f" {key}=\"{value}\""
 
         return props_str
+    
+    def text_node_to_html_node(text_node):
+        match (text_node.text_type):
+            case TextType.TEXT:
+                return LeafNode(None, text_node.text)
+            case TextType.BOLD:
+                return LeafNode("b", text_node.text)
+            case TextType.ITALIC:
+                return LeafNode("i", text_node.text)
+            case TextType.CODE:
+                return LeafNode("code", text_node.text)
+            case TextType.LINK:
+                return LeafNode("a", text_node.text, {"href": text_node.url})
+            case TextType.IMAGE:
+                return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            case _:
+                raise Exception("TextType not in enum")
     
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
