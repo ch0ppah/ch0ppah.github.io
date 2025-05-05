5a166f90bb72b3e8597f74bb88fe86a06a023d20
ch0ppah <nick0suna76@gmail.com>
2025-05-05 11:58:58 -0400

    add regex functions for processing link and image text

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 
@@ -70,6 +70,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" normal", TextType.NORMAL)
             ],
             new_nodes
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-diamond-version/5/5e/Natu.jpg)"
+        )
+        self.assertListEqual([("image", "https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-diamond-version/5/5e/Natu.jpg")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://www.youtube.com/)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://www.youtube.com/")
+            ],
+            matches,
         )
 
 if __name__ == "__main__":
