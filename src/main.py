7072afcb27321abd8a32507f1071fbd971edc965
ch0ppah <nick0suna76@gmail.com>
2025-05-09 17:01:51 -0400

    switch hosting to github pages

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 import os
+import sys
 import shutil
 from pathlib import Path
 from block_markdown import markdown_to_html_node
 
+
+default_basepath = "/"
+
 def main():
-    copy_directory("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    copy_directory("./static", "./docs")
+    generate_pages_recursive("./content", "./template.html", "./docs", basepath)
 
 def copy_directory(source_dir, dest_dir):
     # Removes destination directly, ensures clean copy
@@ -52,7 +60,7 @@ def extract_title(markdown):
     
     raise Exception("h1 header not found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as file:
         markdown = file.read()
@@ -62,20 +70,20 @@ def generate_page(from_path, template_path, dest_path):
     html_string = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     content = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
-
+    content = content.replace("href=\"/", f"href\"{basepath}").replace("src=\"/", f"src\"{basepath}")
     dest_dir = os.path.dirname(dest_path)
     os.makedirs(dest_dir, exist_ok=True)
     with open(dest_path, "w") as file:
         file.write(content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 main()
